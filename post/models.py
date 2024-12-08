c5062ef698a0747cbfaedf2b33deb43465a695ac
Parsa <deadlockfree504@gmail.com>
2024-12-08 11:29:00 +0330

    added Category Model to models.py made a ForeignKey to category removed unncessary post_ variables

--- post/models.py
+++ post/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f" {self.category} "
+
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=100)
-    post_content = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    post_views = models.IntegerField(default=0)
-    post_category = models.CharField(max_length=50)
-    
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #Foreing Key to Category
+
+
     def __str__(self):
-        return f"{self.post_category}: {self.post_title} created at {self.created_at}"
+        return f"{self.category}: {self.title} created at {self.created_at}"
 
 
